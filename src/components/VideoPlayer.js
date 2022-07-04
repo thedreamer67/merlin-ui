ffc8a84f8d4884d5dcadf675fed37fb287e6e455
thedreamer67 <shaunaliting@gmail.com>
2022-07-04 16:29:22 +0800

    add mute and fullscreen buttons

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,14 +1,27 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { findDOMNode } from 'react-dom';
 import ReactPlayer from 'react-player';
+import screenfull from 'screenfull';
 import video from '../assets/food_recipe.mp4';
 import '../styles/VideoPlayer.css';
 
 const VideoPlayer = () => {
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [played, setPlayed] = useState(0);
+	const [controls, setControls] = useState(false);
+	const [isMuted, setIsMuted] = useState(false);
 	const [hasEnded, setHasEnded] = useState(false);
 
 	const ref = useRef(null);
+
+	useEffect(() => {
+		function onFullscreenChange() {
+			setControls(Boolean(document.fullscreenElement));
+		}
+		document.addEventListener('fullscreenchange', onFullscreenChange);
+		return () =>
+			document.removeEventListener('fullscreenchange', onFullscreenChange);
+	}, []);
 
 	const handlePlayPause = () => {
 		setIsPlaying((prev) => !prev);
@@ -38,35 +51,31 @@ const VideoPlayer = () => {
 
 	const handleSeeking = () => {};
 
-	const handleFullscreen = () => {};
+	const handleFullscreen = () => {
+		screenfull.request(findDOMNode(ref.current));
+		setControls(true);
+	};
 
 	const handleEnded = () => {
 		setIsPlaying(false);
 		setHasEnded(true);
 	};
 
-	// const toggleMute = () => {};
+	const toggleMute = () => {
+		setIsMuted((prev) => !prev);
+	};
 
 	return (
-		<div
-			style={{
-				// margin: 'auto',
-				textAlign: 'center',
-				display: 'flex',
-				flexDirection: 'column',
-				justifyContent: 'center',
-				alignItems: 'center',
-				height: '100%',
-			}}
-		>
+		<div className='video-component'>
 			<div>
 				<ReactPlayer
 					className='react-player'
 					width='100%'
 					height='100%'
-					controls={true}
+					controls={controls}
 					playing={isPlaying}
 					played={played}
+					muted={isMuted}
 					url={video}
 					ref={ref}
 					onEnded={handleEnded}
@@ -74,27 +83,37 @@ const VideoPlayer = () => {
 			</div>
 
 			<section className='controls'>
+				<span id='mediaButton' style={{ marginRight: '5rem' }}>
+					{!isMuted && (
+						<i class='fa-solid fa-volume-high' onClick={toggleMute} />
+					)}
+					{isMuted && (
+						<i class='fa-solid fa-volume-xmark' onClick={toggleMute} />
+					)}
+				</span>
+
 				<span id='mediaButton' style={{ marginRight: '0.5rem' }}>
 					<i className='fa-solid fa-backward-step' onClick={handleRewind} />
 				</span>
-				{isPlaying ? (
-					<span id='mediaButton' style={{ marginRight: '0.5rem' }}>
+
+				<span id='mediaButton' style={{ marginRight: '0.5rem' }}>
+					{isPlaying && (
 						<i className='fa-solid fa-pause' onClick={handlePlayPause} />
-					</span>
-				) : (
-					<span id='mediaButton' style={{ marginRight: '0.5rem' }}>
+					)}
+					{!isPlaying && (
 						<i className='fa-solid fa-play' onClick={handlePlayPause} />
-					</span>
-				)}
-				<span id='mediaButton'>
+					)}
+				</span>
+
+				<span id='mediaButton' style={{ marginRight: '5rem' }}>
 					<i className='fa-solid fa-forward-step' onClick={handleForward} />
+				</span>
+
+				<span id='mediaButton'>
+					<i class='fa-solid fa-expand' onClick={handleFullscreen} />
 				</span>
 			</section>
 		</div>
-
-		// <video controls className='react-player' width='100%' height='100%'>
-		// 	<source src={video} type='video/mp4' />
-		// </video>
 	);
 };
 
