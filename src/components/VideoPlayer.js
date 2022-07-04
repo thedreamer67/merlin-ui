e674280f840e0aa2196e7a9fc18eba2583dfa16f
thedreamer67 <shaunaliting@gmail.com>
2022-07-04 15:57:33 +0800

    add play and skip buttons

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,17 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ReactPlayer from 'react-player';
-import video from '../assets/dargo_interview_cut.mp4';
+import video from '../assets/food_recipe.mp4';
 import '../styles/VideoPlayer.css';
 
 const VideoPlayer = () => {
-	const [isPlaying, setIsPlaying] = useState(true);
+	const [isPlaying, setIsPlaying] = useState(false);
 	const [played, setPlayed] = useState(0);
+	const [hasEnded, setHasEnded] = useState(false);
 
-	const handlePlayPause = () => {};
+	const ref = useRef(null);
+
+	const handlePlayPause = () => {
+		setIsPlaying((prev) => !prev);
+		if (hasEnded) {
+			setPlayed(ref.current.getCurrentTime());
+			setHasEnded(false);
+		}
+		console.log(played);
+	};
+
+	const handleForward = () => {
+		let seekTo = ref.current.getCurrentTime() + 10;
+		seekTo =
+			seekTo > ref.current.getDuration()
+				? Math.floor(ref.current.getDuration())
+				: seekTo;
+		setPlayed(seekTo);
+		ref.current.seekTo(seekTo, 'seconds');
+		console.log(seekTo);
+	};
+
+	const handleRewind = () => {
+		const seekTo = ref.current.getCurrentTime() - 10;
+		setPlayed(seekTo);
+		ref.current.seekTo(seekTo, 'seconds');
+	};
 
 	const handleSeeking = () => {};
 
 	const handleFullscreen = () => {};
+
+	const handleEnded = () => {
+		setIsPlaying(false);
+		setHasEnded(true);
+	};
 
 	// const toggleMute = () => {};
 
@@ -32,14 +64,31 @@ const VideoPlayer = () => {
 					className='react-player'
 					width='100%'
 					height='100%'
-					controls={false}
-					playing
+					controls={true}
+					playing={isPlaying}
+					played={played}
 					url={video}
+					ref={ref}
+					onEnded={handleEnded}
 				/>
 			</div>
 
 			<section className='controls'>
-				<i class='fa-solid fa-play' /> hello
+				<span id='mediaButton' style={{ marginRight: '0.5rem' }}>
+					<i className='fa-solid fa-backward-step' onClick={handleRewind} />
+				</span>
+				{isPlaying ? (
+					<span id='mediaButton' style={{ marginRight: '0.5rem' }}>
+						<i className='fa-solid fa-pause' onClick={handlePlayPause} />
+					</span>
+				) : (
+					<span id='mediaButton' style={{ marginRight: '0.5rem' }}>
+						<i className='fa-solid fa-play' onClick={handlePlayPause} />
+					</span>
+				)}
+				<span id='mediaButton'>
+					<i className='fa-solid fa-forward-step' onClick={handleForward} />
+				</span>
 			</section>
 		</div>
 
