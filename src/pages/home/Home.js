10d996cb86131d0eaf857d38f9f22b263c514dbe
Long Chay Boon Stuart (IA) <lchayboo@dso.org.sg>
2022-07-04 11:29:00 +0800

    Add home page

---
+++ src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import icon from '../../images/merlinicon.png';
+import styles from './Home.module.css';
+
+function Home() {
+    return (
+        <div className={styles.home}>
+            <img className={styles.icon} src={icon} alt="merlin"></img>
+            <div className={styles.hometext}>
+                <p className={styles.header}>MERLIN video editor</p>
+                <p className={[styles.start, styles.hvrHang].join(" ")}>Click here to see the magic</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
