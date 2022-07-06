30255676d703b0a437770d7a25f9cab8bd44ffdb
thedreamer67 <shaunaliting@gmail.com>
2022-07-06 15:12:17 +0800

    Playback time updates with scrubbing

--- src/pages/editor/components/videoplayer/VideoPlayer.js
+++ src/pages/editor/components/videoplayer/VideoPlayer.js
@@ -25,12 +25,16 @@ const VideoPlayer = (props) => {
 
 	useEffect(() => {
 		function onScroll() {
-			props.seeking &&
-				// console.log(
-				// 	`videoplayer: useEffect onScroll: props.scrollPosition = ${props.scrollPosition}`
-				// );
-				props.seeking &&
+			// props.seeking &&
+			// console.log(
+			// 	`videoplayer: useEffect onScroll: props.scrollPosition = ${props.scrollPosition}`
+			// );
+			if (props.seeking) {
 				ref.current.seekTo(props.scrollPosition, 'fraction');
+				props.getTime(props.scrollPosition * ref.current.getDuration());
+			}
+			// props.seeking &&
+			// ref.current.seekTo(props.scrollPosition, 'fraction');
 		}
 		document.getElementById('timeline').addEventListener('scroll', onScroll);
 		return () => {
