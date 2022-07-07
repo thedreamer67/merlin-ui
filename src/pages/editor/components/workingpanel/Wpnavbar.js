7bdb142fade27f71c0ce223d892b453ead1c0767
thedreamer67 <shaunaliting@gmail.com>
2022-07-07 14:46:46 +0800

    Improve searchbar

--- src/pages/editor/components/workingpanel/Wpnavbar.js
+++ src/pages/editor/components/workingpanel/Wpnavbar.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react';
+
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 import './styles/Wpnavbar.css';
+
 import Library from './Library';
 import AutoCaption from './AutoCaption';
 import MagicAction from './MagicAction';
@@ -57,6 +66,42 @@ function Wpnavbar(props) {
 	//     setlibraryclick(false);
 	//   }
 	// }, [captionclick]);
+
+	const SearchBar = () => {
+		return (
+			<Box sx={{ '& > :not(style)': { m: 1 } }}>
+				<FormControl variant='standard'>
+					<InputLabel htmlFor='input-with-icon-adornment'>
+						With a start adornment
+					</InputLabel>
+					<Input
+						id='input-with-icon-adornment'
+						startAdornment={
+							<InputAdornment position='start'>
+								<AccountCircle />
+							</InputAdornment>
+						}
+					/>
+				</FormControl>
+				<TextField
+					id='input-with-icon-textfield'
+					label='TextField'
+					InputProps={{
+						startAdornment: (
+							<InputAdornment position='start'>
+								<AccountCircle />
+							</InputAdornment>
+						),
+					}}
+					variant='standard'
+				/>
+				<Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+					<AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+					<TextField id='input-with-sx' label='With sx' variant='standard' />
+				</Box>
+			</Box>
+		);
+	};
 
 	return (
 		<React.Fragment>
